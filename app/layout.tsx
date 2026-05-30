75eb0ab42f66681e3c8652233855fda5fcf64cc9
mustafa-ezzi <mustafaezzi143@gmail.com>
2026-05-30 14:39:44 +0500

    added PWA icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,16 @@ export const metadata: Metadata = {
     title: "Us",
     statusBarStyle: "default",
   },
+  icons: {
+    icon: [
+      { url: "/icon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [
+      { url: "/icon-152x152.png", sizes: "152x152", type: "image/png" },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
